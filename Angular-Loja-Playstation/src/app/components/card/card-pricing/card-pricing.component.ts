d54ed77cadc5132a04b7a6c5838ca751619df205
rrxx <131102811+rrxdg777@users.noreply.github.com>
2023-10-17 12:55:10 -0300

    Add files via upload

---
+++ Angular-Loja-Playstation/src/app/components/card/card-pricing/card-pricing.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-pricing',
+  templateUrl: './card-pricing.component.html',
+  styleUrls: ['./card-pricing.component.css']
+})
+export class CardPricingComponent implements OnInit{
+
+  @Input()
+  gameType:string = "Digital PS5"
+  @Input()
+  gamePrice:string = "R$ 199,90"
+
+  constructor(){}
+  ngOnInit(): void {
+  }
+
+}
---
+++ Angular-Loja-Playstation/src/app/components/card/card.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, OnInit, Renderer2 } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit{
+
+  @Input()
+  gameCover:string = ""
+  @Input()
+  gameLabel:string=""
+  @Input()
+  gameType:string = ""
+  @Input()
+  gamePrice:string = ""
+
+  constructor(private renderer: Renderer2){}
+  ngOnInit(): void {
+  }
+
+}
